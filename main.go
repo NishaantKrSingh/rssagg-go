5e0315d75b8eb0a10e844e99ae509b06535a9ab7
Nishant Singh <nishaantkrsingh@gmail.com>
2025-03-28 03:33:39 +0530

    Added env loader

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main(){
 	fmt.Println("Hello World!")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == ""{
+		log.Fatal("Port not found in the environment!") //Immediately stops code and also outputs the proper date and time
+	}
+	fmt.Println("Port:", portString)
 }
