74cd85fa18acc84d5d079d13fb5b0d79e6749bb0
Nishant Singh <nishaantkrsingh@gmail.com>
2025-03-28 04:55:23 +0530

    basic code to get the server running

--- main.go
+++ main.go
@@ -3,17 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
 	godotenv.Load()
 	portString := os.Getenv("PORT")
-	if portString == ""{
+	if portString == "" {
 		log.Fatal("Port not found in the environment!") //Immediately stops code and also outputs the proper date and time
 	}
-	fmt.Println("Port:", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	serv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	fmt.Printf("Server starting on port %v", portString)
+	err := serv.ListenAndServe()
+	if err != nil {
+		log.Fatal("An error occured", err)
+	}
 }
